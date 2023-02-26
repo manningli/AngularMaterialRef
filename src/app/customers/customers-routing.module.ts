1e8fe31d91d892a7271849c4a9f1b442ce9e566f
Lawrence M <39105751+manningli@users.noreply.github.com>
2023-02-26 15:39:40 -0500

    mat-card

--- src/app/customers/customers-routing.module.ts
+++ src/app/customers/customers-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 import { CutsomerListComponent } from './cutsomer-list/cutsomer-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: CutsomerListComponent,
+  },
+  {
+    path: 'info',
+    component: CustomerCardComponent,
   },
 ];
 
--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CutsomerListComponent } from './cutsomer-list/cutsomer-list.component';
+import { CustomerCardComponent } from './customer-card/customer-card.component';
 
+// Angular Material modules
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [
-    CutsomerListComponent
-  ],
+  declarations: [CutsomerListComponent, CustomerCardComponent],
   imports: [
     CommonModule,
-    CustomersRoutingModule
-  ]
+    CustomersRoutingModule,
+
+    // Material
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+  ],
 })
-export class CustomersModule { }
+export class CustomersModule {}
